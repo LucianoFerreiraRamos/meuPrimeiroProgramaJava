ebd13e44cff223386e3c9e1ceedcdc45f0010a12
Luciano <97047885+LucianoFerreiraRamos@users.noreply.github.com>
2022-02-16 15:30:38 -0300

    meu primeiro programa na linguagem java

---
+++ src/meuprograma/MeuPrograma.java
@@ -0,0 +1,16 @@
+package meuprograma;
+
+public class MeuPrograma {
+	
+	public static void main (String[] args ){
+		
+		
+	System.out.println("Olá, Mundo ! ");
+	System.out.println ("Me Livrei da Maldição !");
+	
+	}
+	
+	
+	
+
+}
